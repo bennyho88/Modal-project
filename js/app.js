4ec4146f3eeb8b798fc558b9e4caf23ea16359a9
Benny Ho <benny.ho2530@gmail.com>
2020-02-25 20:25:57 +0100

    refactored function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,117 @@
+// lightbox
+
+(function() {
+
+  // all images
+
+  let imageList = [];
+  let counter = 0;
+
+  const images = document.querySelectorAll('.store-img');
+  const container = document.querySelector('.lightbox-container');
+  const item = document.querySelector('.lightbox-item');
+  const closeIcon = document.querySelector('.lightbox-close');
+  const btnLeft = document.querySelector('.btnLeft');
+  const btnRight = document.querySelector('.btnRight');
+
+
+  // add all images to the array
+images.forEach(image => {
+
+
+ imageList.push(image.src);
+})
+// open modal
+
+images.forEach(image => {
+
+  image.addEventListener('click', function(event) {
+
+    container.classList.add('show');
+
+    // get source
+
+    let src = event.target.src;
+    // console.log(src);
+
+    counter = imageList.indexOf(src);
+    //console.log(counter)
+
+    // show modal with an image
+
+    item.style.backgroundImage = `url(${src})`;
+
+
+  });
+});
+
+// close modal
+
+closeIcon.addEventListener('click', function() {
+
+  container.classList.remove('show');
+})
+
+// left btn
+btnLeft.addEventListener('click', function() {
+
+  counter--;
+
+  if(counter < 0) {
+    counter = imageList.length - 1;
+  }
+  
+  item.style.backgroundImage = `url(${imageList[counter]})`;
+  console.log('counter - : ' + counter)
+})
+
+//right btn
+btnRight.addEventListener('click', function() {
+
+  counter++;
+
+  if(counter > imageList.length -1) {
+    counter = 0;
+  }
+
+  item.style.backgroundImage = `url(${imageList[counter]})`;
+  console.log('counter + :' + counter);
+});
+
+// console.log(imageList)
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 (function () {
@@ -358,7 +472,7 @@ modalClose.addEventListener('click', function() {
 })()
 */
 
-
+/*
 (function() {
 
 
@@ -439,7 +553,7 @@ lightboxClose.addEventListener('click', function() {
 })();
 
 
-
+*/
 
 
 
